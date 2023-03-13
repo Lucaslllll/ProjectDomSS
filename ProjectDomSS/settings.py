c4222db5837d230ad98d821b17a9f26c4e41995a
Lucaslllll <lucascosta12367@gmail.com>
2023-03-13 00:49:58 +0000

    merge server

--- ProjectDomSS/settings.py
+++ ProjectDomSS/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'ProjectDomSS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'domss',
+        'USER': 'dom',
+        'PASSWORD': 'root1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
