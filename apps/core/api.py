1e7ad2dd520cc058e49b6123598a29f104fef5f1
Lucaslllll <lucascosta12367@gmail.com>
2023-03-12 22:45:34 -0300

    corsheaders middleware

--- apps/core/api.py
+++ apps/core/api.py
@@ -11,6 +11,18 @@ class ProviderViewSet(viewsets.ModelViewSet):
     queryset = Provider.objects.all()
     serializer_class = AllSerializer
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({})
+
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
